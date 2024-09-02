2a781ec257d362cd3f83b65c290390dcd115e63f
uditya kumar <rishi95122@gmail.com>
2024-09-02 13:55:36 +0530

    added applyidCard component

---
+++ src/components/ApplyIdcard/IdCardComponents.jsx/IdCard.jsx
@@ -0,0 +1,13 @@
+
+const IdCard = () => {
+  return (
+    <div className='flex flex-col  justify-around'>
+        <div className='flex flex-col justify-center items-center'>
+            <img src="/idcard.png" className="max-md:h-96 max-md:w-80 "/>
+        </div>
+     
+    </div>
+  )
+}
+
+export default IdCard

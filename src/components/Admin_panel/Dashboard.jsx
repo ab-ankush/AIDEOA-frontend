a0bf9b6182f834e4c85ad9b0cc1f2356cd63da93
Ankush Bhagat <ankusha062@gmail.com>
2024-09-05 14:17:41 +0530

    Some checks done

--- src/components/Admin_panel/Dashboard.jsx
+++ src/components/Admin_panel/Dashboard.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import Header from './Header.jsx'
+import React from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header.jsx";
 const Dashboard = () => {
-    return (
-        <div className=' grid  grid-cols-[1fr_19fr] xl: xl:w-screen xl:h-screen '>
-            <Sidebar />
-            <div>
-                <Header />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="grid  grid-cols-[1fr_19fr] min-h-screen">
+      <Sidebar />
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;

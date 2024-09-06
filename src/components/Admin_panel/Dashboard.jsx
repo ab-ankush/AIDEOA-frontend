6bb74ecb643231fdc2ae0effda8ee937353c01f2
Ankush Bhagat <ankusha062@gmail.com>
2024-09-06 16:20:06 +0530

    changed admin pannel

--- src/components/Admin_panel/Dashboard.jsx
+++ src/components/Admin_panel/Dashboard.jsx
@@ -6,11 +6,11 @@ import Table from "./Table.jsx";
 import Transactions from "../../adminpanel/Transactions";
 const Dashboard = () => {
   return (
-    <div className=" grid  grid-cols-[1fr_19fr] w-screen h-screen ">
+    <div className="grid grid-cols-[1fr_19fr] w-screen h-screen bg-gray-100 ">
       <Sidebar />
       <div>
         <Header />
-        <div className=" w-[90%] border border-gray-400 h-[600px] ml-7 m-5 p-4 shadow-lg">
+        <div className="bg-white w-[90%]  h-[600px] ml-7 m-5 p-4 shadow-lg rounded-lg">
           {/* <Chart /> */}
           <Table />
         </div>

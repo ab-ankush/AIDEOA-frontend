2b5737f0cd7cfc50eb5ffd7d23fb26ca08e10d73
Harshit Gupta <harshitgupta9235@gmail.com>
2024-09-06 20:34:42 +0530

    updated

--- src/components/Admin_panel/common/Header.jsx
+++ src/components/Admin_panel/common/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className='flex overflow-x-hidden justify-between items-center p-[10px]'>
             <h1 className='text-[30px] m-5'>Dashboard</h1>
             <div className='flex justify-center items-center my-[20px] px-[10px] pr-[30px]'>
-                <img src="images/logo.png" className='w-[44px] h-[44px] rounded-full' alt="" />
+                {/* <img src="images/logo.png" className='w-[44px] h-[44px] rounded-full' alt="" /> */}
                 <p className='text-[25px]'>Anna</p>
             </div>
         </div>

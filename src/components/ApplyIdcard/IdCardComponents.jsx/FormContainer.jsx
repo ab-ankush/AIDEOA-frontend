575487b39a899878dd1d3d50001278796aaf34e8
Harshit Gupta <harshitgupta9235@gmail.com>
2024-09-10 13:39:06 +0530

    updated

--- src/components/ApplyIdcard/IdCardComponents.jsx/FormContainer.jsx
+++ src/components/ApplyIdcard/IdCardComponents.jsx/FormContainer.jsx
@@ -8,45 +8,54 @@ const FormContainer = () => {
   const [formType, SetFormType] = useState(true);
   const [submit, setsubmit] = useState(false)
   return (
-    <div className="my-20 relative mx-32 flex flex-col gap-6 max-xl:mx-8 max-lg:mx-8 max-md:mx-4 max-sm:mx-2 ">
-      <p className="text-xl font-medium">ID Card form</p>
-      <div className="flex justify-center w-full  ">
-        <div className="p-1 border-2 flex border-gray-200 max-w-full  rounded-full">
-          <button
-            className={`w-72 h-10 rounded-full  text-sm text-black  font-medium ${formType && "membershipBtn text-white"
-              }`}
-            onClick={() => SetFormType(true)}
-          >
-            Student ID
-          </button>
-          <button
-            className={`w-72 h-10 rounded-full  text-sm text-black font-medium ${!formType && "membershipBtn text-white"
-              }`}
-            onClick={() => SetFormType(false)}
-          >
-            Employee ID
-          </button>
-        </div>
-      </div>
-      <div className="flex  w-full justify-between gap-16 max-md:flex-col max-md:flex-col-reverse max-lg:gap-10 max-lg:">
-        {
-          formType ? <IDform /> : <EmployeeForm />
-        }
-
-        <IdCard />
-
-      </div>
-      <div className=" flex ">
-        <button className="w-52 max-sm:w-full h-12 membershipBtn rounded-2xl text-white font-medium text-lg">
-          Submit
-        </button>
-      </div>
+    <div>
       {
-        submit ? <div className=" flex ">
-          <button className="w-52 max-sm:w-full h-12 membershipBtn rounded-2xl text-white font-medium text-lg">
-            Download Id Card
-          </button>
-        </div> : ""
+        submit ? (<div className="my-20 relative mx-32 flex flex-col gap-6 max-xl:mx-8 max-lg:mx-8 max-md:mx-4 max-sm:mx-2">
+          <div className="">
+            <IdCard />
+          </div>
+
+          <div className=" flex ">
+            <button className="w-52 max-sm:w-full h-12 membershipBtn rounded-2xl text-white font-medium text-lg">
+              Download Id Card
+            </button>
+          </div>
+
+        </div>) : (<div className="my-20 relative mx-32 flex flex-col gap-6 max-xl:mx-8 max-lg:mx-8 max-md:mx-4 max-sm:mx-2 ">
+          <p className="text-xl font-medium">ID Card form</p>
+          <div className="flex justify-center w-full  ">
+            <div className="p-1 border-2 flex border-gray-200 max-w-full  rounded-full">
+              <button
+                className={`w-72 h-10 rounded-full  text-sm text-black  font-medium ${formType && "membershipBtn text-white"
+                  }`}
+                onClick={() => SetFormType(true)}
+              >
+                Student ID
+              </button>
+              <button
+                className={`w-72 h-10 rounded-full  text-sm text-black font-medium ${!formType && "membershipBtn text-white"
+                  }`}
+                onClick={() => SetFormType(false)}
+              >
+                Employee ID
+              </button>
+            </div>
+          </div>
+          <div className="flex  w-full justify-between gap-16 max-md:flex-col max-md:flex-col-reverse max-lg:gap-10 max-lg:">
+            {
+              formType ? <IDform /> : <EmployeeForm />
+            }
+
+            <IdCard />
+
+          </div>
+          <div className=" flex ">
+            <button className="w-52 max-sm:w-full h-12 membershipBtn rounded-2xl text-white font-medium text-lg">
+              Submit
+            </button>
+          </div>
+
+        </div>)
       }
     </div>
   );
--- src/components/Cards/EmailNotiCard.jsx
+++ src/components/Cards/EmailNotiCard.jsx
@@ -3,19 +3,19 @@ const EmailNotiCard = () => {
   return (
     <div className="px-24 max-md:px-12 max-sm:px-6 ">
       <div className="container mx-auto p-20 my-12 bg-gray-100 flex items-center gap-8 justify-between min-h-64 max-lg:flex-col max-lg:py-10 max-lg:gap-4 rounded-2xl text-center max-md:px-16 max-md:px-6">
-        <h1 className='font-semibold text-3xl  max-md:text-2xl'>
-        GET READY TO KNOW THE LATEST UPDATE!
-        </h1>
+        <h3 className='font-semibold text-3xl  max-md:text-2xl'>
+          GET READY TO KNOW THE LATEST UPDATE!
+        </h3>
         <p className='font-normal text-base max-lg:text-sm '>
-        Join us for our latest upcoming events by subscribing us & get notified..
+          Join us for our latest upcoming events by subscribing us & get notified..
         </p>
         <div className='relative'>
-            <input className='w-80 h-14  rounded-full focus:outline-none px-4 pb-1 max-sm:w-64' 
+          <input className='w-80 h-14  rounded-full focus:outline-none px-4 pb-1 max-sm:w-64'
             placeholder='Enter email address'
-            />  
-            <button className='bg-purple-600  absolute text-sm  right-0 top-0 pb-1 text-white h-full w-24 rounded-full  font-normal max-sm:w-20 max-sm:text-xs'>continue</button>
+          />
+          <button className='bg-purple-600  absolute text-sm  right-0 top-0 pb-1 text-white h-full w-24 rounded-full  font-normal max-sm:w-20 max-sm:text-xs'>continue</button>
         </div>
-    </div>
+      </div>
     </div>
   )
 }

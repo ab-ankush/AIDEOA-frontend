92ba4980e7cd8220e36c9c7ffcec70ba49fd22bc
uditya kumar <rishi95122@gmail.com>
2024-09-11 21:54:39 +0530

    updation

--- src/components/Eventpage/EventPageComponents/Card/Card.jsx
+++ src/components/Eventpage/EventPageComponents/Card/Card.jsx
@@ -1,18 +1,17 @@
 import { IoLocationOutline } from "react-icons/io5";
 import { MdCalendarMonth } from "react-icons/md";
-import { CiShare1 } from "react-icons/ci";
+
 const Card = () => {
   return (
-    <div className=" flex flex-col m-auto w-[100%] lg:w-[80%] xl:w-[70%] 2xl:w-[70%] mb-24  shadow-md items-center justify-center shadow-gray-600 rounded-2xl">
-    {/* <div className="  m-auto max-w-lg mb-24 shadow-md  shadow-gray-600 rounded-2xl"> */}
+    <div className="  m-auto max-w-lg mb-24 shadow-md  shadow-gray-600 rounded-2xl">
       <div className="flex justify-between  px-4 lg:px-8 w-full  pt-5 pb-5 bg-customgradient-background rounded-t-2xl">
         
-          <p className="font-semibold w-[60%] text-white  text-3xl ">
+          <p className="font-semibold w-full text-white w-14 text-3xl ">
             DAY 2
           </p>
       
-        <div className="size-3 w-[34%]">
-          <p className="text-xs text-center text-white">
+        <div className="size-3 w-36">
+          <p className="text-xs text-white">
             March 18th, 2025 <br />
             <span>10:00AM - 5:00 PM</span>
           </p>
@@ -22,7 +21,6 @@ const Card = () => {
         <div className="text-xl font-semibold">
           <p>AIDEOA Hosts Successful Annual Conference</p>
         </div>
-        <div className="flex gap-2 items-center">
         <div className="flex flex-col justify-between h-10">
           <div className="flex text-sm">
             <IoLocationOutline size={15} />
@@ -33,9 +31,6 @@ const Card = () => {
             <p className="ml-1 font-light">2:00 PM - 1:00 PM</p>
           </div>
         </div>
-        <CiShare1 size={22} className="cursor-pointer"/>
-        </div>
-        
         <div className="text-sm font-light pb-5">
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting

282317a88f630c4c9b6e64ce9ea2a9935bb0a6fd
uditya kumar <rishi95122@gmail.com>
2024-09-16 21:46:20 +0530

    done

--- src/components/Eventpage/EventPageComponents/Card/Card.jsx
+++ src/components/Eventpage/EventPageComponents/Card/Card.jsx
@@ -1,49 +1,51 @@
 import { IoLocationOutline } from "react-icons/io5";
 import { MdCalendarMonth } from "react-icons/md";
 
-const Card = () => {
+const Card = ({item}) => {
+  const whatsappShareData=(item)=>{
+  const {title,location}=item
+    const encodedText = encodeURIComponent(`${
+      location + "  "+ title
+    }`);
+    const shareUrl = `https://api.whatsapp.com/send?text=${encodedText}`;
+    window.open(shareUrl, '_blank');
+  }
   return (
     <div className="  m-auto max-w-lg mb-24 shadow-md  shadow-gray-600 rounded-2xl">
       <div className="flex justify-between  px-4 lg:px-8 w-full  pt-5 pb-5 bg-customgradient-background rounded-t-2xl">
         
           <p className="font-semibold w-full text-white w-14 text-3xl ">
-            DAY 2
+          {item?.head}
           </p>
       
         <div className="size-3 w-36">
           <p className="text-xs text-white">
-            March 18th, 2025 <br />
-            <span>10:00AM - 5:00 PM</span>
+            {item.date} <br />
+            <span>{item.time}</span>
           </p>
         </div>
       </div>
       <div className="flex flex-col m-auto justify-between gap-6 p-4 lg:p-8">
         <div className="text-xl font-semibold">
-          <p>AIDEOA Hosts Successful Annual Conference</p>
+          <p>{item.title}</p>
         </div>
         <div className="flex flex-col justify-between h-10">
           <div className="flex text-sm">
             <IoLocationOutline size={15} />
-            <p className="ml-1 font-light">Grand Ballon Hotel</p>
+            <p className="ml-1 font-light">{item.location}</p>
           </div>
           <div className="flex text-sm">
             <MdCalendarMonth size={15} />
-            <p className="ml-1 font-light">2:00 PM - 1:00 PM</p>
+            <p className="ml-1 font-light">{item.time}</p>
           </div>
         </div>
         <div className="text-sm font-light pb-5">
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survive. Lorem
-            Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of ..
+          {item.description}
           </p>
         </div>
         <div>
-          <button className="bg-purple-600 text-white px-4 py-3 rounded-xl ">Share</button>
+          <button onClick={()=>whatsappShareData(item)} className="bg-purple-600 text-white px-4 py-3 rounded-xl ">Share</button>
         </div>
       </div>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBAKXMgoIlHbCszOmqG7TElG-ZukDn4xYQ",
-  authDomain: "aideoa-e13b1.firebaseapp.com",
-  projectId: "aideoa-e13b1",
-  storageBucket: "aideoa-e13b1.appspot.com",
-  messagingSenderId: "661421011915",
-  appId: "1:661421011915:web:3472d309fe8fddac04bc4e"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth =getAuth()
 export const db=getFirestore(app)
